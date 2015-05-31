f52e333043f62c87bcd8e67f3724a3e71d03a9b7
onewaystreet <onewaystreet@outlook.com>
2015-05-31 19:05:56 +0800

    加入部分服务端更新代码

---
+++ Tts.TMusic.Updates.Web/Controllers/ServiceUpdateController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Tts.TMusic.Updates.Web.Controllers
+{
+    public class ServiceUpdateController : Controller
+    {
+        public string GetLastVersion()
+        {
+            var path = Server.MapPath("~ServiceVersion.xml");
+            XDocument xdoc = XDocument.Load(path);
+            var v = xdoc.Element("File").Element("Version").Value.Trim();
+            return v;
+        }
+
+        public ActionResult Download()
+        {
+            var path = Server.MapPath("~/ServiceUpdates/last.zip");
+            return new FilePathResult(path, "application/x-zip-compressed");
+        }
+    }
+}
---
+++ Tts.TMusic.Updates.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tts.TMusic.Updates.Web.MvcApplication" Language="C#" %>
