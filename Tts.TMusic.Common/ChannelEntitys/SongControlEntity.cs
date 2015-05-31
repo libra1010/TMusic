e947e41b08059a7e91596fb9c03f28b9e9f43f25
onewaystreet <onewaystreet@outlook.com>
2015-05-31 17:29:22 +0800

    重新提交实体

---
+++ Tts.TMusic.Common/ChannelEntitys/SongControlEntity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tts.TMusic.Common.ChannelEntitys
+{
+    /// <summary>
+    /// 音乐播放传输实体
+    /// </summary>
+    [Serializable]
+    public class SongControlEntity
+    {
+        /// <summary>
+        /// 播放曲目ID
+        /// </summary>
+        public int? SongID { get; set; }
+
+        /// <summary>
+        /// 播放列表ID
+        /// </summary>
+        public int? PlayListID { get; set; }
+    }
+}
