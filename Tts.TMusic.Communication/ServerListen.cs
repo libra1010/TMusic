bb3c17fc9cb1c2a3eff8f84c34aba5f9edda8697
onewaystreet <onewaystreet@outlook.com>
2015-05-31 17:58:04 +0800

    为毛这个VS总是提交不上去呀
    为毛这个VS总是提交不上去呀

---
+++ Tts.TMusic.Communication/ServerListen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tts.AsyncSocket.AsyncSocketServer;
+
+namespace Tts.TMusic.Service.Server
+{
+    public class ServerListen
+    {
+        private IServerSocket _server;
+        
+        public void Start() 
+        {
+            _server = new IServerSocket(10, 1024 * 4);
+            _server.Init();
+            _server.Start("0.0.0.0", 2112);
+            _server.OnClientConnect += _server_OnClientConnect;
+            _server.OnClientRead += _server_OnClientRead;
+            _server.OnClientDisconnect += _server_OnClientDisconnect;
+        }
+
+        void _server_OnClientDisconnect(object sender, AsyncSocket.AsyncUserToken e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void _server_OnClientRead(object sender, AsyncSocket.AsyncUserToken e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void _server_OnClientConnect(object sender, AsyncSocket.AsyncUserToken e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
