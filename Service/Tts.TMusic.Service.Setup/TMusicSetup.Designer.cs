e62d7c559612acbfcd1c30268ec8958cd1dc3904
onewaystreet <onewaystreet@outlook.com>
2015-05-31 18:50:06 +0800

    又见该死的VS
    又见该死的VS

---
+++ Service/Tts.TMusic.Service.Setup/TMusicSetup.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Tts.TMusic.Service.Setup
+{
+    partial class TMusicSetup
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSetup = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSetup
+            // 
+            this.btnSetup.Location = new System.Drawing.Point(91, 171);
+            this.btnSetup.Name = "btnSetup";
+            this.btnSetup.Size = new System.Drawing.Size(75, 23);
+            this.btnSetup.TabIndex = 0;
+            this.btnSetup.Text = "安装";
+            this.btnSetup.UseVisualStyleBackColor = true;
+            this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(89, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // TMusicSetup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSetup);
+            this.Name = "TMusicSetup";
+            this.Text = "TMusic服务安装";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetup;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Service/Tts.TMusic.Service.Setup/TMusicSetup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tts.TMusic.Common;
+
+namespace Tts.TMusic.Service.Setup
+{
+    public partial class TMusicSetup : Form
+    {
+        public TMusicSetup()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSetup_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() => 
+            {
+                string file = System.IO.Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, "Tts.TMusic.Service.exe");
+                if (ServiceHelper.ServiceIsExisted("TMusicService")) 
+                {
+                    if (MessageBox.Show("TMusicService 服务已经安装,需要卸载重新安装吗？", "Ary you ok", MessageBoxButtons.YesNo) == System.Windows.Forms.DialogResult.No)
+                        return;
+
+                    if (ServiceHelper.IsServiceStart("TMusicService"))
+                        ServiceHelper.StopService("TMusicService");
+                   
+                    ServiceHelper.UnInstallService(file,"TMusicService");
+                }
+
+                ServiceHelper.InstallService(null, file, "TMusicService");
+                MessageBox.Show("服务安装完成");
+            });
+        }
+    }
+}
